04baf17af4669de5070280ff054694e0910069ca
mhrazib121 <mhrazib121@gmail.com>
2022-08-12 18:23:14 +0600

    express initialize monogodb connect and route added

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://devchamber:wyBqNZPfytMn7Cp1@cluster0.xvulc.mongodb.net/?retryWrites=true&w=majority"
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const post = require('./routes/api/post');
+
+// MongoDB config 
+const db = require('./config/keys').mongoURI;
+
+// MongoDB connect
+mongoose.connect(db)
+    .then(() => console.log('MongoDB connected'))
+    .catch(error => console.log(error));
+
+// use routes 
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/post', post);
+
+app.get('/', (req, res) => {
+    res.send("Hello World from the server site of Devchamber");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
